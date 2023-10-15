7a4caafdf476e49b04cc60f96adbaab1f3561c66
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-10-15 11:34:38 +0100

    User class id added

--- src/main/java/com/resourciumoptima/resourcium_optima/models/entities/User.java
+++ src/main/java/com/resourciumoptima/resourcium_optima/models/entities/User.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Employe {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
