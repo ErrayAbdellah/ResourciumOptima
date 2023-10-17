e9100eee2db30d571881d9f31200e88c4e19c17b
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-10-17 11:29:37 +0100

    add repo

---
+++ src/main/java/com/resourciumoptima/resourcium_optima/repositories/Repo.java
@@ -0,0 +1,9 @@
+package com.resourciumoptima.resourcium_optima.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Repo {
+
+}
